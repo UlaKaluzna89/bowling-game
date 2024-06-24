607888d37e34799f07f094cdb01ab9d5bbb5e23f
UlaKaluzna89 <122965482+UlaKaluzna89@users.noreply.github.com>
2024-06-24 09:49:24 +0200

    core

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GameComponent} from "./game/game.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/bowling-game', pathMatch: 'full' },
+  { path: 'bowling-game', component: GameComponent },
+  { path: '**', redirectTo: 'not-found' },
+  { path: 'not-found', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { GameComponent } from './game/game.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatCardModule} from "@angular/material/card";
+import {AppMaterialModule} from "./app-material.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    GameComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
